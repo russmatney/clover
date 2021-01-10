cc97f7fd10cdff00d7f92f74c157eb95e2248174
Russell Matney <russell.matney@gmail.com>
2021-01-10 01:27:12 -0500

    feat: minimize window options, filter args, fix clover command
    The electron process needs some args passed to allow for transparency on
    linux. Those args need to be filtered so url and title are found
    properly.
    
    This updates the main function to filter those args, and removes some
    unnecessary window options.

--- src/clover/core.cljs
+++ src/clover/core.cljs
@@ -12,38 +12,48 @@
   (println "init-browser" opts)
   (reset! main-window
           (browser-window
-            {:transparent     true
-             :backgroundColor "#00000000"
-             :titleBarStyle   "hidden"
-             :frame           false
-             :thickFrame      false
-             :type            "splash"
-             :title           (str "clover/" (or title "no-title"))
+            {:transparent true
+             :frame       false
+             :type        "splash"
+             :title       title
 
              :webPreferences
-             {:enableRemoteModule         false
-              :nodeIntegrationInSubFrames false
-              :nodeIntegration            false
-              ;; :enableRemoteModule         true
-              ;; :nodeIntegrationInSubFrames true
-              ;; :nodeIntegration            true
-              }}))
-  ;; Path is relative to the compiled js file (main.js in our case)
-  (.loadURL ^js @main-window url)
+             {:enableRemoteModule false
+              :nodeIntegration    false}}))
 
+  (.loadURL ^js @main-window url)
   (println "main-window reset" opts)
   (.on ^js @main-window "closed" #(reset! main-window nil)))
 
 (defn main [& args]
-  (println "Starting clover frame with params:" args)
-  (let [url   (some-> args first)
-        title (nth args 2 nil)]
-
+  (println "Starting clover" args)
+  (let [args  (->> args
+                   rest ;; drop clover.js
+                   (remove (fn [s]
+                             (or
+                               (.startsWith s "--")
+                               (= s "clover.js")))))
+        url   (some-> args first)
+        title (nth args 2 "no-title")]
     (.on app "window-all-closed" #(.quit app))
     (.on app "ready"
          (fn []
            (let [pause 1000]
              (println "app ready")
              (js/setTimeout
-               #(init-browser {:title title :url url})
+               #(init-browser {:title (str "clover/" title) :url url})
                pause))))))
+
+(comment
+  (->> ["--hello" "world"]
+       (filter #(.startsWith % "--")))
+
+  (let [v "hello"]
+    (.startsWith v "he")
+    )
+
+  (println "hello")
+  (init-browser
+    {:title "Mah title"
+     :url   ""})
+  )
